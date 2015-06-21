0f5b2d11c9ba6fb491d60accb550062e34facc09
Thomass07 <rtamas79p@gmail.com>
2015-06-21 21:19:43 +1000

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@ Training_X=read.table("./train/X_train.txt")
 Training_Y=read.table("./train/y_train.txt") #which activities were performed
 
 Activity_labels=read.table("activity_labels.txt")
- colnames(Activity_labels)<-c("ActivityId","Activity") #Column names to Activity Labels
+	colnames(Activity_labels)<-c("ActivityId","Activity") #Column names to Activity Labels
  
 		Training <- cbind(cbind(Training_X,Training_Y),Training_subject) #Create the training dataset
 
@@ -17,12 +17,11 @@ Test_Y=read.table("./test/y_test.txt") #which activities were performed
 		Test <- cbind(cbind(Test_X,Test_Y),Test_subject) #Create the test dataset
 
 Dataset <- rbind(Training, Test)  #Create the Dataset
+
 #------------------TASK 3 and 4-----------------------------
 features <- read.table("features.txt",stringsAsFactors=FALSE)[[2]]
-
-#features = gsub('[-()]', '', features)
-colnames(Dataset) <- features
-colnames(Dataset)[562:563] <- c("ActivityId","Subject")
+	colnames(Dataset) <- features
+	colnames(Dataset)[562:563] <- c("ActivityId","Subject")
 
 Activity_Subject<-Dataset[562:563] #Activity and Subject Rows to keep
 
@@ -31,13 +30,14 @@ meanstd <- grep("-(mean|std)\\(\\)", features)
 Dataset <- Dataset[, meanstd]
 Dataset2 <- cbind(Dataset,Activity_Subject)
 
-names(Dataset2) <- gsub("-","",names(Dataset2))
-names(Dataset2) <- gsub("\\(\\)","",names(Dataset2))
+	names(Dataset2) <- gsub("-","",names(Dataset2))
+	names(Dataset2) <- gsub("\\(\\)","",names(Dataset2))
+
 #----------------------------------------------
-Dataset2 <- data.frame(Dataset2) #Convert the tables to dataframes
+Dataset2 <- data.frame(Dataset2) #Convert tables to dataframes
 Activity_labels <- data.frame(Activity_labels) 
 Dataset2 <- merge(Dataset2,Activity_labels,by.x = "ActivityId", by.y ="ActivityId") #Merge
-Dataset2 <- subset(Dataset2, select= -ActivityId) #I do not want to keep the ActivityId
+Dataset2 <- subset(Dataset2, select= -ActivityId)
 			
 #------------------TASK 5------------------------------
 Dataset2<- melt(Dataset2, id=c("Subject", "Activity"))
