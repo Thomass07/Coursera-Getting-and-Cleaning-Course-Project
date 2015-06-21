b6797fda987a3c18a371d8889ec0bb48f0f4b00c
Thomass07 <rtamas79p@gmail.com>
2015-06-21 21:15:36 +1000

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+library(reshape2)
 
 #------------------TASK 1-----------------------------
 Training_subject=read.table("./train/subject_train.txt")
@@ -5,42 +6,42 @@ Training_X=read.table("./train/X_train.txt")
 Training_Y=read.table("./train/y_train.txt") #which activities were performed
 
 Activity_labels=read.table("activity_labels.txt")
- colnames(Activity_labels)<-c("ActivityId","Activity")
+ colnames(Activity_labels)<-c("ActivityId","Activity") #Column names to Activity Labels
  
-		Training <- cbind(cbind(Training_X,Training_Y),Training_subject)
+		Training <- cbind(cbind(Training_X,Training_Y),Training_subject) #Create the training dataset
 
 Test_subject=read.table("./test/subject_test.txt")
 Test_X=read.table("./test/x_test.txt")
 Test_Y=read.table("./test/y_test.txt") #which activities were performed
 
-		Test <- cbind(cbind(Test_X,Test_Y),Test_subject)
+		Test <- cbind(cbind(Test_X,Test_Y),Test_subject) #Create the test dataset
 
-Dataset <- rbind(Training, Test)
+Dataset <- rbind(Training, Test)  #Create the Dataset
 #------------------TASK 3 and 4-----------------------------
 features <- read.table("features.txt",stringsAsFactors=FALSE)[[2]]
 
 #features = gsub('[-()]', '', features)
 colnames(Dataset) <- features
-
 colnames(Dataset)[562:563] <- c("ActivityId","Subject")
 
-Activity_Subject<-Dataset[562:563]
+Activity_Subject<-Dataset[562:563] #Activity and Subject Rows to keep
 
 #------------------TASK 2-----------------------------
 meanstd <- grep("-(mean|std)\\(\\)", features)
 Dataset <- Dataset[, meanstd]
 Dataset2 <- cbind(Dataset,Activity_Subject)
 
+names(Dataset2) <- gsub("-","",names(Dataset2))
+names(Dataset2) <- gsub("\\(\\)","",names(Dataset2))
 #----------------------------------------------
-Dataset2 <- data.frame(Dataset2)
-Activity_labels <- data.frame(Activity_labels)
-Dataset2 <- merge(Dataset2,Activity_labels,by.x = "ActivityId", by.y ="ActivityId")
-Dataset2<- subset(Dataset2, select= -ActivityId)
-
+Dataset2 <- data.frame(Dataset2) #Convert the tables to dataframes
+Activity_labels <- data.frame(Activity_labels) 
+Dataset2 <- merge(Dataset2,Activity_labels,by.x = "ActivityId", by.y ="ActivityId") #Merge
+Dataset2 <- subset(Dataset2, select= -ActivityId) #I do not want to keep the ActivityId
+			
 #------------------TASK 5------------------------------
-#I did not have time to do it
-...
-...
-...
+Dataset2<- melt(Dataset2, id=c("Subject", "Activity"))
+Dataset2<- dcast(Dataset2, Subject+Activity ~ variable, mean)
+dim(Dataset2) #180*68
 
-write.table(Dataset2, "results.txt", row.name=FALSE)
+write.table(Dataset2, "Final_result.txt", row.name=FALSE)
